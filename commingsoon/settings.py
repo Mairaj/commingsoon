3262b531aca155a6445aee5adde34c3fff90baa6
mairaj <mairajkhan00@gmail.com>
2015-08-07 14:49:46 +0530

    eight commit

--- commingsoon/settings.py
+++ commingsoon/settings.py
@@ -20,7 +20,7 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static-root')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static-root'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Quick-start development settings - unsuitable for production

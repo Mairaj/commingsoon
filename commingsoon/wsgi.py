da4ed52d7b4a40970a5121ae0e0fe4cbde09ab0d
mairaj <mairajkhan00@gmail.com>
2015-08-07 13:05:32 +0530

    Fourth commit

--- commingsoon/wsgi.py
+++ commingsoon/wsgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commingsoon.settings")
 
@@ -17,3 +18,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commingsoon.settings")
 #application = django.core.handlers.wsgi.WSGIHandler()
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

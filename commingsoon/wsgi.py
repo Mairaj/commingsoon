f34e0ba07cbcf22ab42507b8c3e0c4baba891705
mairaj <mairajkhan00@gmail.com>
2015-08-07 12:09:47 +0530

    Third commit

--- commingsoon/wsgi.py
+++ commingsoon/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-#from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commingsoon.settings")
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
 
-#application = get_wsgi_application()
+application = get_wsgi_application()

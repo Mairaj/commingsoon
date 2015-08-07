a9dd296310917fa46f8f869e3102d940093b3c9a
mairaj <mairajkhan00@gmail.com>
2015-08-07 17:22:00 +0530

    test

--- commingsoon/settings.py
+++ commingsoon/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static-root')
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media-root')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (

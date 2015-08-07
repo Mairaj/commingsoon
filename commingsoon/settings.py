8ddfb4bde0d7e1e8203139d6beab9db50e79057f
mairaj <mairajkhan00@gmail.com>
2015-08-07 11:56:43 +0530

    Second Commit

--- commingsoon/settings.py
+++ commingsoon/settings.py
@@ -75,21 +75,21 @@ WSGI_APPLICATION = 'commingsoon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
-     'default':{
-               'ENGINE': 'django.db.backends.mysql',
-               'NAME' : 'commingsoon',
-               'USER' : 'root',
-               'PASSWORD': 'root',
-               }
-            }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+#DATABASES = {
+#     'default':{
+#               'ENGINE': 'django.db.backends.mysql',
+#               'NAME' : 'commingsoon',
+#               'USER' : 'root',
+#               'PASSWORD': 'root',
+#               }
+#            }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
